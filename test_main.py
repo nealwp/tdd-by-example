e24964b2c86f85951edade780616337906a87517
wprestonneal <mrprestonneal@gmail.com>
2022-08-29 20:40:52 -0700

    implement Franc class

--- test_main.py
+++ test_main.py
@@ -4,19 +4,30 @@ class Dollar:
     
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
-    
-    def equals(self, dollar):
-        return self._amount == dollar._amount
+
+    def __eq__(self, __o: object) -> bool:
+        return self._amount == __o._amount
+
+class Franc:
+    def __init__(self, amount) -> None:
+        self._amount = amount
+
+    def times(self, multiplier):
+        return Franc(self._amount * multiplier)
+
+    def __eq__(self, __o: object) -> bool:
+        return self._amount == __o._amount
+
 def test_multiplication():
     five = Dollar(5)
-    assert Dollar(10).equals(five.times(2))
-    assert Dollar(15).equals(five.times(3))
+    assert Dollar(10) == five.times(2)
+    assert Dollar(15) == five.times(3)
 
 def test_franc_multiplication():
     five = Franc(5)
-    assert Franc(10).equals(five.times(2))
-    assert Franc(15).equals(five.times(3))
+    assert Franc(10) == five.times(2)
+    assert Franc(15) == five.times(3)
 
 def test_equality():
-    assert Dollar(5).equals(Dollar(5))
-    assert not Dollar(5).equals(Dollar(6))
+    assert Dollar(5) == Dollar(5)
+    assert not Dollar(5) == Dollar(6)
