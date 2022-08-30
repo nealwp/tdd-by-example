f3600cd29d66143f6cf1adf5a4698777b76dc818
wprestonneal <mrprestonneal@gmail.com>
2022-08-30 16:38:02 -0700

    remove subclasses for franc and dollar, complete representation in Money superclass.

--- test_main.py
+++ test_main.py
@@ -4,35 +4,22 @@ class Money:
         self._currency = currency
 
     def __eq__(self, __o: object) -> bool:
-        return self._amount == __o._amount and type(self) == type(__o)
+        return self._amount == __o._amount and self._currency == __o._currency
+
+    def __str__(self) -> str:
+        return self._amount + " " + self._currency
 
     def times(self, multiplier):
-        return Money(self._amount * multiplier)
+        return Money(self._amount * multiplier, self._currency)
     
     def dollar(amount):
-        return Dollar(amount, "USD")
+        return Money(amount, "USD")
     
     def franc(amount):
-        return Franc(amount, "CHF")
+        return Money(amount, "CHF")
 
     def currency(self):
         return self._currency
-
-class Dollar(Money):
-
-    def __init__(self, amount, currency):
-        super().__init__(amount, currency)
-    
-    def times(self, multiplier):
-        return Money.dollar(self._amount * multiplier)
-
-class Franc(Money):
-
-    def __init__(self, amount, currency):
-        super().__init__(amount, currency)
-
-    def times(self, multiplier):
-        return Money.franc(self._amount * multiplier)
 
 def test_multiplication():
     five = Money.dollar(5)
@@ -45,10 +32,12 @@ def test_multiplication():
 def test_equality():
     assert Money.dollar(5) == Money.dollar(5)
     assert not Money.dollar(5) == Money.dollar(6)
-    assert Money.franc(5) == Money.franc(5)
-    assert not Money.franc(5) == Money.franc(6)
     assert not Money.franc(5) == Money.dollar(5)
 
 def test_currency():
     assert "USD" == Money.dollar(1).currency()
     assert "CHF" == Money.franc(1).currency()
+
+def test_simple_addition():
+    sum = Money.dollar(5) + Money.dollar(5)
+    assert sum == Money.dollar(10)
