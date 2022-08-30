069d2819aaf8aa6c78778716b2e7807f8e2981ff
wprestonneal <mrprestonneal@gmail.com>
2022-08-29 21:24:43 -0700

    add currency property to Franc

--- test_main.py
+++ test_main.py
@@ -3,22 +3,41 @@ class Money:
         self._amount = amount
 
     def __eq__(self, __o: object) -> bool:
-        return self._amount == __o._amount
+        return self._amount == __o._amount and type(self) == type(__o)
+
+    def times(self, multiplier):
+        return Money(self._amount * multiplier)
+    
+    def dollar(amount):
+        return Dollar(amount)
+    
+    def franc(amount):
+        return Franc(amount)
 
 class Dollar(Money):
     
     def times(self, multiplier):
         return Dollar(self._amount * multiplier)
 
+    def currency(self):
+        return "USD"
+
 class Franc(Money):
+
+    def __init__(self, amount):
+        self._amount = amount
+        self._currency = "CHF"
 
     def times(self, multiplier):
         return Franc(self._amount * multiplier)
 
+    def currency(self):
+        return "CHF"
+
 def test_multiplication():
-    five = Dollar(5)
-    assert Dollar(10) == five.times(2)
-    assert Dollar(15) == five.times(3)
+    five = Money.dollar(5)
+    assert Money.dollar(10) == five.times(2)
+    assert Money.dollar(15) == five.times(3)
 
 def test_franc_multiplication():
     five = Franc(5)
@@ -26,7 +45,12 @@ def test_franc_multiplication():
     assert Franc(15) == five.times(3)
 
 def test_equality():
-    assert Dollar(5) == Dollar(5)
-    assert not Dollar(5) == Dollar(6)
-    assert Franc(5) == Franc(5)
-    assert not Franc(5) == Franc(6)
+    assert Money.dollar(5) == Money.dollar(5)
+    assert not Money.dollar(5) == Money.dollar(6)
+    assert Money.franc(5) == Money.franc(5)
+    assert not Money.franc(5) == Money.franc(6)
+    assert not Money.franc(5) == Money.dollar(5)
+
+def test_currency():
+    assert "USD" == Money.dollar(1).currency()
+    assert "CHF" == Money.franc(1).currency()
