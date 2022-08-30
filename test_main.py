ca4555e0d6b323ef930fff73a2c6b480f74a34d1
wprestonneal <mrprestonneal@gmail.com>
2022-08-29 20:31:44 -0700

    add Franc test

--- test_main.py
+++ test_main.py
@@ -1,12 +1,22 @@
 class Dollar:
     def __init__(self, amount) -> None:
-        self.amount = amount
+        self._amount = amount
     
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
-def test_multipication():
+        return Dollar(self._amount * multiplier)
+    
+    def equals(self, dollar):
+        return self._amount == dollar._amount
+def test_multiplication():
     five = Dollar(5)
-    product = five.times(2)
-    assert 10 == product.amount
-    product = five.times(3)
-    assert 15 == product.amount
+    assert Dollar(10).equals(five.times(2))
+    assert Dollar(15).equals(five.times(3))
+
+def test_franc_multiplication():
+    five = Franc(5)
+    assert Franc(10).equals(five.times(2))
+    assert Franc(15).equals(five.times(3))
+
+def test_equality():
+    assert Dollar(5).equals(Dollar(5))
+    assert not Dollar(5).equals(Dollar(6))
