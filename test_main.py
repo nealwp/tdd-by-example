801d6fd5ce7fa061099300d7dd003f6d549aa106
wprestonneal <mrprestonneal@gmail.com>
2022-08-30 21:14:45 -0700

    add Bank class. Move exchange rate responsibility from Money to Bank

--- test_main.py
+++ test_main.py
@@ -1,11 +1,14 @@
-class ExpressionInterface:
+class Expression:
     pass
 
 class Bank:
-    def reduce(self, source, to):
-        return Money.dollar(10)
+    def reduce(self, source: Expression, to: str):
+        return source.reduce(self, to)
 
-class Money(ExpressionInterface):
+    def rate(self, conv_from: str, conv_to: str):
+        return 2 if conv_from == "CHF" and conv_to == "USD" else 1
+
+class Money(Expression):
     def __init__(self, amount, currency) -> None:
         self._amount = amount
         self._currency = currency
@@ -17,7 +20,7 @@ class Money(ExpressionInterface):
         return self._amount + " " + self._currency
 
     def __add__(self, addend):
-        return Money(self._amount + addend._amount, self._currency)
+        return Sum(self._amount, addend._amount, self._currency)
 
     def times(self, multiplier):
         return Money(self._amount * multiplier, self._currency)
@@ -31,7 +34,18 @@ class Money(ExpressionInterface):
     def currency(self):
         return self._currency
 
+    def reduce(self, bank: Bank, to: str):
+        rate = bank.rate(self._currency, to)
+        return Money(self._amount / rate, to)
 
+class Sum(Expression):
+    def __init__(self, augend: Money, addend: Money) -> None:
+       self._augend = augend
+       self._addend = addend
+
+    def reduce(self, bank, to):
+        amount = self._augend._amount + self._addend._amount
+        return Money(amount, to) 
 
 def test_multiplication():
     five = Money.dollar(5)
@@ -52,7 +66,30 @@ def test_currency():
 
 def test_simple_addition():
     five = Money.dollar(5)
-    sum = five + five
+    sum = Sum(five, five)
     bank = Bank()
     reduced = bank.reduce(sum, "USD")
     assert reduced == Money.dollar(10)
+
+def test_reduce_sum():
+    sum = Sum(Money.dollar(3), Money.dollar(4))
+    bank = Bank()
+    result = bank.reduce(sum, "USD")
+    assert Money.dollar(7) == result
+
+def test_plus_returns_sum():
+    five = Money.dollar(5)
+    sum = Sum(five, five)
+    assert five == sum._augend
+    assert five == sum._addend
+
+def test_reduce_money():
+    bank = Bank()
+    result = bank.reduce(Money.dollar(1), "USD")
+    assert Money.dollar(1) == result
+
+def test_reduce_money_different_currency():
+    bank = Bank()
+    bank.add_rate("CHF", "USD", 2)
+    result = bank.reduce(Money.franc(2), "USD")
+    assert Money.dollar(1) == result
