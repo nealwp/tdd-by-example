efebaadddb6d362365bcd09eae208fbe0fa503b5
nealwp <mrprestonneal@gmail.com>
2025-03-01 12:56:37 -0800

    first equality test

--- go/main.go
+++ go/main.go
@@ -5,3 +5,7 @@ type Dollar struct{ amount int }
 func (d *Dollar) Times(multiplier int) Dollar {
 	return Dollar{d.amount * multiplier}
 }
+
+func (d *Dollar) Equals(dollar Dollar) bool {
+	return true
+}
--- go/main_test.go
+++ go/main_test.go
@@ -8,10 +8,21 @@ func assertEquals(t *testing.T, expected int, got int) {
 	}
 }
 
+func assertTrue(t *testing.T, r bool) {
+	if !r {
+		t.Error("expected true, got false")
+	}
+}
+
 func TestMultiplication(t *testing.T) {
 	five := Dollar{5}
 	product := five.Times(2)
 	assertEquals(t, 10, product.amount)
 	product = five.Times(3)
 	assertEquals(t, 15, product.amount)
+}
+
+func TestEquality(t *testing.T) {
+	dollar := Dollar{5}
+	assertTrue(t, dollar.Equals(Dollar{5}))
 }
