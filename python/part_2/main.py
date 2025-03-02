c3bf680509ca1d4ae7a8ebd2325678b21977a0eb
nealwp <mrprestonneal@gmail.com>
2025-03-02 11:57:23 -0800

    extract test case class

--- python/part_2/main.py
+++ python/part_2/main.py
@@ -1,14 +1,19 @@
-class WasRun:
+class TestCase:
     def __init__(self, name: str):
-        self.was_run = None
         self.name = name
-
-    def test_method(self):
-        self.was_run = 1
 
     def run(self):
         method = getattr(self, self.name)
         method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name: str):
+        self.was_run = None
+        TestCase.__init__(self, name=name)
+
+    def test_method(self):
+        self.was_run = 1
 
 
 test = WasRun("test_method")
