1d7f150b768d5f80a9c8fa42569d8e5273be859f
nealwp <mrprestonneal@gmail.com>
2025-03-02 12:36:05 -0800

    simplify test case test

--- python/part_2/main.py
+++ python/part_2/main.py
@@ -2,26 +2,40 @@ class TestCase:
     def __init__(self, name: str):
         self.name = name
 
+    def setup(self):
+        pass
+
     def run(self):
+        self.setup()
         method = getattr(self, self.name)
         method()
 
 
 class WasRun(TestCase):
     def __init__(self, name: str):
-        self.was_run = None
+        self.was_run = False
         TestCase.__init__(self, name=name)
 
     def test_method(self):
-        self.was_run = 1
+        pass
+
+    def setup(self):
+        self.was_run = True
+        self.was_setup = True
 
 
 class TestCaseTest(TestCase):
+    def setup(self):
+        self.test = WasRun("test_method")
+
+    def test_setup(self):
+        self.test.run()
+        assert self.test.was_setup
+
     def test_running(self):
-        test = WasRun("test_method")
-        assert not test.was_run
-        test.run()
-        assert test.was_run
+        self.test.run()
+        assert self.test.was_run
 
 
 TestCaseTest("test_running").run()
+TestCaseTest("test_setup").run()
